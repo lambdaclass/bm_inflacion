cae3831a150c7e442110776b36ce06339f62c36e
Camilo Plata <camilo1704@gmail.com>
2019-12-27 11:56:21 -0300

    Add files via upload

--- request.py
+++ request.py
@@ -17,3 +17,11 @@ def request_base_monetaria(from_date="2003-01-30",to_date=date.today()):
     r = requests.post(url,data=form_data)
     return r.text
 
+def request_base_monetaria_circulante(from_date="2003-01-30",to_date=date.today()):
+    url="http://45.235.96.100/PublicacionesEstadisticas/Principales_variables_datos.asp"
+    form_data = {"fecha_desde":[from_date,None],"fecha_hasta":[to_date,None],"B1":"Enviar",
+            "primeravez":1,"serie":251,"serie1":0,"serie2":0,"serie3":0,"serie4":0,"detalle":"Circulación+monetaria+(en+millones+de+pesos)"}
+    r = requests.post(url,data=form_data)
+    return r.text
+
+
