412bd2a6ff678395e6e3a283c0f975f4af5da0e2
Camilo Plata <camilo1704@gmail.com>
2019-12-27 11:55:00 -0300

    Add files via upload

--- plotting.py
+++ plotting.py
@@ -18,18 +18,13 @@ def plot(df,start,end,month_mean=False):
     ax.tick_params(axis="x", labelsize=12, labelrotation=40)
     ax.tick_params(axis="y", labelsize=12)
     df['Dates'] = pd.to_datetime(df['Dates'], format='%d/%m/%Y')    
-    if month_mean:
-        name=df.columns[1]
-        df=df.reset_index().set_index('Dates').resample('1M').mean()
-        df.reset_index(level=0, inplace=True)
-        df.rename(columns={df.columns[1]:name},inplace = True)
+    
     start_date=pd.to_datetime(start, format='%Y-%m-%d')
     end_date=pd.to_datetime(end, format='%Y-%m-%d')
    
     mask = (df['Dates'] > start_date) & (df['Dates'] <= end_date)
     df=df.loc[mask]
-    #x_data=mdates.datestr2num(df['Dates'])
-    #ax.set_xlim(x_data[0],x_data[-1])
+    
     ax.grid(alpha=0.3)
     
     ax.set_xlabel(df.columns[0],fontweight='bold',fontsize='x-large')
